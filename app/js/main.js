09d93dd1a3469948371324524a13883eb2e2f646
VaheAA <vahe.abov@gmail.com>
2021-11-27 20:50:37 +0400

    final

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('#menu-btn');
 const mobileMenu = document.querySelector('#menu');
 const body = document.querySelector('body');
+const accordionBtns = document.querySelectorAll('.title--xsm');
+const menuLists = document.querySelectorAll('.footer__menu-list');
 
 function navToggle() {
     btn.classList.toggle('open');
@@ -8,4 +10,14 @@ function navToggle() {
     body.classList.toggle('no-scroll');
 }
 
+
+accordionBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        menuLists.forEach((list, index2) => {
+            if (index === index2) {
+                list.classList.toggle('open');
+            }
+        });
+    });
+});
 btn.addEventListener('click', navToggle);
